b0f71991c257649d79293f35ef6082e3d175fd6d
Kevin Robert Keegan <kevin@krkeegan.com>
2022-08-12 08:44:56 -0700

    Revert ":page_facing_up: Add Toggle to Enable Automatic Raw Log Uploading"
    This reverts commit bbfcbc5e63f628c69f60b25cda3f1641508df4c9.

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -53,12 +53,6 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
       "../assets/offroad/icon_metric.png",
     },
     {
-      "UploadRaw",
-      tr("Upload Raw Logs"),
-      tr("Upload full logs and full resolution video by default while on Wi-Fi. If not enabled, individual logs can be marked for upload at useradmin.comma.ai."),
-      "../assets/offroad/icon_network.png",
-    },
-    {
       "RecordFront",
       tr("Record and Upload Driver Camera"),
       tr("Upload data from the driver facing camera and help improve the driver monitoring algorithm."),
