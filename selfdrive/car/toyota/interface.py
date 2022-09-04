7ebab70e794d643194dbb9c598e4cd9fffd824f2
Kevin Robert Keegan <kevin@krkeegan.com>
2022-09-04 16:11:30 -0700

    :b: Allow Sport Gear Use; Enables Engine Braking on Downhill

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -11,6 +11,7 @@ ButtonType = car.CarState.ButtonEvent.Type
 FrogPilotButtonType = custom.FrogPilotCarState.ButtonEvent.Type
 EventName = car.CarEvent.EventName
 SteerControlType = car.CarParams.SteerControlType
+GearShifter = car.CarState.GearShifter
 
 
 class CarInterface(CarInterfaceBase):
@@ -181,7 +182,7 @@ class CarInterface(CarInterfaceBase):
       ]
 
     # events
-    events = self.create_common_events(ret)
+    events = self.create_common_events(ret, extra_gears=[GearShifter.sport])
 
     # Lane Tracing Assist control is unavailable (EPS_STATUS->LTA_STATE=0) until
     # the more accurate angle sensor signal is initialized
