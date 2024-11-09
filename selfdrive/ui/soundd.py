60add53092cbef3f71fe51589b0c776fa338d2e6
Kevin Robert Keegan <kevin@krkeegan.com>
2024-11-09 13:10:34 -0800

    :sound: Decrease Auto Volume Levels

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -19,12 +19,12 @@ from openpilot.selfdrive.frogpilot.frogpilot_variables import ACTIVE_THEME_PATH,
 SAMPLE_RATE = 48000
 SAMPLE_BUFFER = 4096 # (approx 100ms)
 MAX_VOLUME = 1.0
-MIN_VOLUME = 0.1
+MIN_VOLUME = 0.01
 CONTROLS_TIMEOUT = 5 # 5 seconds
 FILTER_DT = 1. / (micd.SAMPLE_RATE / micd.FFT_SAMPLES)
 
-AMBIENT_DB = 30 # DB where MIN_VOLUME is applied
-DB_SCALE = 30 # AMBIENT_DB + DB_SCALE is where MAX_VOLUME is applied
+AMBIENT_DB = 37 # DB where MIN_VOLUME is applied
+DB_SCALE = 35 # AMBIENT_DB + DB_SCALE is where MAX_VOLUME is applied
 
 AudibleAlert = car.CarControl.HUDControl.AudibleAlert
 
