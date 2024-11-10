fa21b191f4e9506bac298d6ade95c01c84c37c46
Comma Device <device@comma.ai>
2024-11-10 11:39:30 -0800

    Decrease Minumum Sound Level

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -20,7 +20,7 @@ from openpilot.selfdrive.frogpilot.frogpilot_variables import get_frogpilot_togg
 SAMPLE_RATE = 48000
 SAMPLE_BUFFER = 4096 # (approx 100ms)
 MAX_VOLUME = 1.0
-MIN_VOLUME = 0.1
+MIN_VOLUME = 0.01
 CONTROLS_TIMEOUT = 5 # 5 seconds
 FILTER_DT = 1. / (micd.SAMPLE_RATE / micd.FFT_SAMPLES)
 
